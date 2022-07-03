818e213d00529926cf9614ecdfa5a25a18e6388a
iamrajabli <iamrajabli@outlook.com>
2022-07-03 19:57:18 +0400

    Products completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,24 @@
-const burger = document.querySelector('.burger__menu .fa-bars'),
-    menu = document.querySelector('#navbar ul');
+const burgerNavbar = document.querySelector('#navbar .burger__menu .fa-bars'),
+    menuNavbar = document.querySelector('#navbar ul'),
+    burgerProducts = document.querySelector('#products .burger__menu .fa-bars'),
+    menuProducts = document.querySelector('#products ul'),
+    productsContent = document.querySelector('#products .products__content');
 
-burger.addEventListener('click', () => {
-   menu.classList.toggle('d-flex')
-})
+
+function burger(selector, menu) {
+    selector.addEventListener('click', () => {
+        menu.classList.toggle('d-flex');
+        selector.style.transition = 'all 200ms linear';
+        selector.classList.toggle('rotate-180');
+    });
+}
+
+function marginTop(selector, menu) {
+    selector.addEventListener('click', () => {
+        menu.classList.toggle('mt-10');
+    });
+}
+
+burger(burgerNavbar, menuNavbar)
+burger(burgerProducts, menuProducts)
+marginTop(burgerProducts, productsContent)
