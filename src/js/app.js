100a2b4a15840ec1daea0d73dc7fbe7ba3a731a4
iamrajabli <iamrajabli@outlook.com>
2022-07-03 14:06:46 +0400

    Project updated

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,5 @@ const burger = document.querySelector('.burger__menu .fa-bars'),
     menu = document.querySelector('#navbar ul');
 
 burger.addEventListener('click', () => {
-    if (menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+   menu.classList.toggle('d-flex')
 })
